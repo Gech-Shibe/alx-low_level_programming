65a4a498597fb800701f7f704779e09530a5d56d
get <gechsh6@gmail.com>
2023-02-16 13:34:17 -0800

    printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints by printf function
+ * Return: 0 (scored)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
+
