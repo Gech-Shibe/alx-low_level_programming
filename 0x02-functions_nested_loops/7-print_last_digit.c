09a10e440f89491c49c3c074a3a5f60f900069d8
get <gechsh6@gmail.com>
2023-02-22 10:35:54 -0800

    7-last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit -  prints the last digit of a number
+ * @a: is a number
+ * Return: the value of the last digit
+ */
+
+int print_last_digit(int a)
+{
+	int y = a % 10;
+
+	if (y < 0)
+		y *= -1;
+	_putchar(y + '0');
+	return (0);
+}
