a05fa5650db966bdbff9b77f92703b1fcce63b33
get <gechsh6@gmail.com>
2023-02-23 10:52:31 -0800

    0-main.c

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,12 @@
+#ifndef main_h
+#define main_h
+#include <stdio.h>
+
+
+void positive_or_negative(int Z);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int d, int m, int y);
+int convert_day(int d, int m);
+
+
+#endif
