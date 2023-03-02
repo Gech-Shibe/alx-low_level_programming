589d2d64f669dabded0a6312714e40d7f7199875
get <gechsh6@gmail.com>
2023-03-02 13:02:13 -0800

    102-magic

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	int a[2];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+	/**
+	 * write a line of code
+	 * main - magic
+	 * ou are not allowed to use the variable
+	 * a in your new line of code
+	 * ou are not allowed to modify the variable p
+	 * Your code should be written at line 19, before the ;
+	 */
+	*(p + s) = 98;
+	/*...prints 98\n*/
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
