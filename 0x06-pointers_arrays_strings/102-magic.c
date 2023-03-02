4a69135374cf4a710b66a779ac0937259c48ae10
get <gechsh6@gmail.com>
2023-03-02 13:08:07 -0800

    102-magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -3,14 +3,14 @@
 int main(void)
 {
 	int n;
-	int a[2];
+	int a[5];
 	int *p;
 
 	a[2] = 1024;
 	p = &n;
 	/**
 	 * write a line of code
-	 * main - magic
+	 * main - magic code
 	 * ou are not allowed to use the variable
 	 * a in your new line of code
 	 * ou are not allowed to modify the variable p
