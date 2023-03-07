af8d430987ae33b6218fe93bb011ff0a264566e9
get <gechsh6@gmail.com>
2023-03-07 12:20:45 -0800

    8-print_diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -15,7 +15,7 @@ void print_diagsums(int *a, int size)
 	int sum1 = 0;
 	int sum2 = 0;
 
-	for (y = 0; y <= (size * size) - size; n + size - 1)
+	for (y = 0; y <= (size * size); y = y + size + 1)
 		sum1 = sum1 + a[y];
 	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
 		sum2 = sum2 + a[n];
