00bf87908892580a1ec70069f5c90b228de871d0
get <gechsh6@gmail.com>
2023-03-07 03:24:58 -0800

    2-strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - A function that locates a character in a string
+ * @s: string
+ * @c: character c
+ * Return: s if the charrcter occurance c, not found NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int y;
+
+	for (y = 0; s[y] >= '\0'; y++)
+	{
+		if (s[y] == c)
+			return (s + y);
+	}
+	return (NULL);
+}
