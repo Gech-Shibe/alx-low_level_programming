54dae1633247408cf6ab05dc79becab4db2e194f
get <gechsh6@gmail.com>
2023-03-09 13:25:00 -0800

    5-sqrt_recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,22 +3,19 @@
 /**
  * _sqrt_recursion - A function returns the natural square root of a number
  * @n: number
- * @z: number
  * Return: radical n, unless -1
  */
 
-int _sqrt_recursion(int n, int z)
+int _sqrt_recursion(int n)
 {
-	if (n == 0 || n == 1)
-	{
-		return (n);
-	}
-	else if (z * z < n)
-		return (_sqrt_recursion(z + 1, n));
-	else if (z * z == n)
-	{
-		return (z);
-	}
-	return (-1);
-}
+	int z = 0;
 
+	if (z < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (_sqrt_recursion(z, n));
+	}
+}
